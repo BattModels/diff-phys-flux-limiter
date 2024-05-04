9e1a6980fd062e3f493e3ff4fd0deea624fb2f99
Chenyang Huang <chyhuang@umich.edu>
2024-05-04 16:40:43 -0400

    add koren limiter; add torch version for minmod and superbee

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,11 +4,20 @@ import torch
 def minmod(r):
     return np.maximum(0, np.minimum(1, r))
 
+def minmod_torch(r):
+    return torch.maximum(torch.tensor([0.]), torch.minimum(torch.tensor([1.]), r))
+
 def superbee(r):
     return np.maximum(np.maximum(0, np.minimum(2 * r, 1)), np.minimum(r, 2))
 
+def superbee_torch(r):
+    return torch.maximum(torch.maximum(torch.tensor([0.]), torch.minimum(2 * r, torch.tensor([1.]))), torch.minimum(r, torch.tensor([2.])))
+
 def vanLeer(r):
     return (r + np.abs(r)) / (1 + np.abs(r))
+
+def koren(r):
+    return np.maximum(0, np.minimum(2*r, np.minimum((1+2*r)/3, 2)))
 
 def FOU(r):
     return np.zeros_like(r)
@@ -46,6 +55,21 @@ def square_wave(N=1000, L=1):
     dx = L / N
     x0 = (np.arange(N)+0.5)*dx
     u0 = np.where(np.logical_and(x0 >= 0.25, x0 <= 0.75), 1., 0.)
+    return x0, u0
+
+def ramp(slope_ratio, N=100, L=1):
+    dx = L / N
+    x0 = (np.arange(N)+0.5)*dx
+    start = 0.25*L
+    mid = x0[int(np.floor((N+1)/2)-1)]
+    end = 0.75*L
+    mid_state = 1.
+    left_slope = mid_state / (0.25*L)
+    right_slope = left_slope / slope_ratio
+    left_ramp = mid_state + left_slope * (x0 - mid)
+    right_ramp = mid_state + right_slope * (x0 - mid)
+    ramp = np.where(x0 <= mid, left_ramp, right_ramp)
+    u0 = np.where(np.logical_and(x0 >= start, x0 <= end), ramp, 0.)
     return x0, u0
 
 def linear_spike(N=1000, L=1):
